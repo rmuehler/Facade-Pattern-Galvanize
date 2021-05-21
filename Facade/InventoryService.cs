ad0107758c4fbd154c25e9137518475d218a5083
Robert Muehler <robertmuehler@gmail.com>
2021-05-21 15:36:12 -0400

    initial commit

---
+++ Facade/InventoryService.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+
+namespace Facade
+{
+    public class InventoryService
+    {
+        private List<Product> products;
+
+
+        void addProduct(Product product)
+        {
+            products.Add(product);
+        }
+
+        void removeProduct(Product product)
+        {
+            products.Remove(product);
+        }
+    }
+}
